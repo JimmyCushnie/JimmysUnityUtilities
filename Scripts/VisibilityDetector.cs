0aeefaff87caed155d0ffd2138c2acf897206e95
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-06-08 22:05:07 -0400

    Added VisibilityDetector

---
+++ Scripts/VisibilityDetector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JimmysUnityUtilities
+{
+    [RequireComponent(typeof(Renderer))]
+    public class VisibilityDetector : MonoBehaviour
+    {
+        public bool IsVisible { get; private set; }
+
+        public event Action OnBecomeVisible;
+        public event Action OnBecomeInvisible;
+
+        private void OnBecameVisible()
+        {
+            IsVisible = true;
+            OnBecomeVisible?.Invoke();
+        }
+
+        private void OnBecameInvisible()
+        {
+            IsVisible = false;
+            OnBecomeInvisible?.Invoke();
+        }
+    }
+}
+
