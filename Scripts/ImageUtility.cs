4a4333b78cbaab9736568c8a33a04b29faf1b1e7
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-09-14 23:27:36 -0400

    Optimized memory usage of textures loaded from disk

--- Scripts/ImageUtility.cs
+++ Scripts/ImageUtility.cs
@@ -38,16 +38,29 @@ namespace JimmysUnityUtilities
         /// Load a PNG or JPG from disk into a Texture2D.
         /// </summary>
         /// <param name="path">The full, rooted path of the file.</param>
-        /// <returns>Null if there's no file at the provided path.</returns>
-        public static Texture2D LoadImageFromDisk(string path)
+        /// <param name="compressLoadedTextureInMemory">Whether to compress the loaded texture in memory.</param>
+        /// <param name="markLoadedTextureReadOnly">If true, the loaded texture will be read-only, and use much less memory.</param>
+        /// <param name="useMipMaps">Whether the loaded texture should have mipmaps.</param>
+        /// <returns>Null if there's no file at the provided path or if the file failed to load.</returns>
+        public static Texture2D LoadImageFromDisk(string path, bool compressLoadedTextureInMemory = true, bool markLoadedTextureReadOnly = true)
         {
             if (!File.Exists(path))
                 return null;
 
-            var bytes = File.ReadAllBytes(path);
-            var texture = new Texture2D(2, 2);
-            texture.LoadImage(bytes);
 
+            // See https://docs.unity3d.com/ScriptReference/ImageConversion.LoadImage.html for compression details
+            // See https://docs.unity3d.com/ScriptReference/Texture2D-ctor.html for non-compression details
+            TextureFormat format = compressLoadedTextureInMemory ? TextureFormat.DXT1 : TextureFormat.RGBA32;
+
+            // idk why but making the texture use mipmaps causes a fuckton of dx11 errors
+            const bool useMipMaps = false;
+
+            var bytes = File.ReadAllBytes(path);
+            var texture = new Texture2D(2, 2, format, useMipMaps);
+
+            if (!texture.LoadImage(bytes, markLoadedTextureReadOnly))
+                return null;
+            
             return texture;
         }
     }
