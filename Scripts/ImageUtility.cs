5eac6d9c74dc153d0709818bb520279f6819be63
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-09-15 16:02:49 -0400

    Revert "never mind, I give up"
    This reverts commit 4aebdd27b81c6a609f4e9d6e1a71360e11d13de2.

--- Scripts/ImageUtility.cs
+++ Scripts/ImageUtility.cs
@@ -38,21 +38,50 @@ namespace JimmysUnityUtilities
         /// Load a PNG or JPG from disk into a Texture2D.
         /// </summary>
         /// <param name="path">The full, rooted path of the file.</param>
-        /// <param name="markLoadedTextureReadOnly">If true, the loaded texture will be read-only, and use much less memory.</param>
         /// <returns>Null if there's no file at the provided path or if the file failed to load.</returns>
-        public static Texture2D LoadImageFromDisk(string path, bool markLoadedTextureReadOnly = true)
+        public static Texture2D LoadImageFromDisk(string path, LoadOptions options = new LoadOptions())
         {
             if (!File.Exists(path))
                 return null;
 
 
-            var bytes = File.ReadAllBytes(path);
-            var texture = new Texture2D(2, 2);
+            // See https://docs.unity3d.com/ScriptReference/ImageConversion.LoadImage.html for compression details
+            // See https://docs.unity3d.com/ScriptReference/Texture2D-ctor.html for non-compression details
+            TextureFormat format = options.CompressLoadedTextureInMemory ? TextureFormat.DXT1 : TextureFormat.RGBA32;
 
-            if (!texture.LoadImage(bytes, markLoadedTextureReadOnly))
+            var bytes = File.ReadAllBytes(path);
+            var texture = new Texture2D(2, 2, format, options.UseMipMaps);
+
+            if (!texture.LoadImage(bytes, options.MarkLoadedTextureReadOnly))
                 return null;
             
             return texture;
+        }
+
+        /// <summary>
+        /// Options for <see cref="LoadImageFromDisk(string, LoadOptions)"/>
+        /// </summary>
+        public struct LoadOptions
+        {
+            public bool MarkLoadedTextureReadOnly;
+            public bool UseMipMaps;
+            public bool CompressLoadedTextureInMemory;
+
+            /// <summary>
+            /// Create a new instance of <see cref="LoadOptions"/>
+            /// </summary>
+            /// <param name="markLoadedTextureReadOnly">If true, the loaded texture will be read-only, and use much less memory.</param>
+            /// <param name="useMipMaps">Whether the loaded texture should have mipmaps.</param>
+            /// <param name="compressLoadedTextureInMemory">Whether to compress the loaded texture in memory. This cannot be true while useMipMaps is also true.</param>
+            public LoadOptions(bool markLoadedTextureReadOnly = true, bool useMipMaps = true, bool compressLoadedTextureInMemory = false)
+            {
+                if (useMipMaps && compressLoadedTextureInMemory)
+                    throw new System.Exception($"{nameof(useMipMaps)} and {nameof(compressLoadedTextureInMemory)} cannot both be true simultaneously"); // Sorry, graphics APIs are stupid
+
+                this.MarkLoadedTextureReadOnly = markLoadedTextureReadOnly;
+                this.UseMipMaps = useMipMaps;
+                this.CompressLoadedTextureInMemory = compressLoadedTextureInMemory;
+            }
         }
     }
 }
