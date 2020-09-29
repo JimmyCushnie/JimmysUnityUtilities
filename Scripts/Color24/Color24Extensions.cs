ddacedac20bf97442ef4c270b0d669459c8090b1
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-09-29 16:36:21 -0400

    added extension to nicely convert a Color24 to HSV

---
+++ Scripts/Color24/Color24Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+using UnityEngine;
+
+namespace JimmysUnityUtilities
+{
+    public static class Color24Extensions
+    {
+        public static void ToHSV(this Color24 color, out float hue, out float saturation, out float value)
+            => Color.RGBToHSV(color.WithOpacity(), out hue, out saturation, out value);
+    }
+}
