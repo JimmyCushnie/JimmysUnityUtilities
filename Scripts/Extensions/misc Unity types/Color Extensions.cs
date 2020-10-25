ba0f8d3cf3fb08517ca30e7e773530a32a80bdec
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-10-25 15:34:05 -0400

    added SetAlpha extensions for color types

--- Scripts/Extensions/misc Unity types/Color Extensions.cs
+++ Scripts/Extensions/misc Unity types/Color Extensions.cs
@@ -2,18 +2,23 @@
 
 namespace JimmysUnityUtilities
 {
-    public static class Color32Extensions
+    public static class ColorExtensions
     {
         public static Color24 WithoutOpacity(this Color32 rgba)
             => new Color24(rgba.r, rgba.g, rgba.b);
-    }
 
-    public static class ColorExtensions
-    {
         public static Color24 WithoutOpacity(this Color rgba)
         {
             var color32 = (Color32)rgba;
             return color32.WithoutOpacity();
         }
+
+
+
+        public static Color32 SetAlpha(this Color32 rgba, byte newAlpha)
+            => new Color32(rgba.r, rgba.g, rgba.b, newAlpha);
+
+        public static Color SetAlpha(this Color rgba, float newAlpha)
+            => new Color(rgba.r, rgba.g, rgba.b, newAlpha);
     }
 }
