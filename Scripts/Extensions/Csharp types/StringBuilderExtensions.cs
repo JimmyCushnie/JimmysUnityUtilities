6ece80bb1f935c7658eeea71fe5253ead88c25ce
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-11-14 18:25:18 -0500

    added some StringBuilderExtensions

---
+++ Scripts/Extensions/Csharp types/StringBuilderExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace JimmysUnityUtilities
+{
+    public static class StringBuilderExtensions
+    {
+        public static int IndexOf(this StringBuilder builder, string find, int startIndex = 0)
+        {
+            int maxSearchLength = builder.Length - find.Length + 1;
+
+            int findIndex;
+            for (int i = startIndex; i < maxSearchLength; ++i)
+            {
+                if (builder[i] == find[0])
+                {
+                    findIndex = 1;
+                    while (findIndex < find.Length && builder[i + findIndex] == find[findIndex])
+                        findIndex++;
+
+                    if (findIndex == find.Length)
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int CountInstancesOf(this StringBuilder builder, string find)
+        {
+            int count = 0;
+
+            int nextIndex = 0;
+            while (nextIndex > -1)
+            {
+                nextIndex = builder.IndexOf(find, nextIndex + find.Length);
+
+                if (nextIndex > -1)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
