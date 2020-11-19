5a447d9066dfdee1c084dee5f518fdb3ac7840ef
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-11-18 22:40:37 -0500

    add char.IsWhitespaceOrNonBreakingSpace extension

---
+++ Scripts/Extensions/Csharp types/CharExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace JimmysUnityUtilities
+{
+    public static class CharExtensions
+    {
+        public static bool IsWhitespaceOrNonBreakingSpace(this char value)
+            => Char.IsWhiteSpace(value) || value == '\u200B';
+    }
+}
