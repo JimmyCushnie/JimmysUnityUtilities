c1c7698fa9160cd28c20a402123c2aac9ee4703f
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-12-01 14:15:21 -0500

    add Plane.PointFromPositionToPlane extension

---
+++ Scripts/Extensions/misc Unity types/PlaneExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace JimmysUnityUtilities
+{
+    public static class PlaneExtensions
+    {
+        public static Vector3 PointFromPositionToPlane(this Plane plane, Vector3 position)
+        {
+            Vector3 planePoint = plane.ClosestPointOnPlane(position);
+            return planePoint - position;
+        }
+    }
+}
