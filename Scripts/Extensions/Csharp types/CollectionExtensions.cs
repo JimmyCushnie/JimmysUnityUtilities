76c4af994158f76a827a69096b6b6a79c57c202b
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-01-24 01:33:19 -0500

    added some more extension methods

--- Scripts/Extensions/Csharp types/CollectionExtensions.cs
+++ Scripts/Extensions/Csharp types/CollectionExtensions.cs
@@ -180,5 +180,12 @@ namespace JimmysUnityUtilities
 
             return list.Count - 1;
         }
+
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+                action.Invoke(item);
+        }
     }
 }
---
+++ Scripts/Extensions/Csharp types/NumberCollectionExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JimmysUnityUtilities
+{
+    public static class NumberCollectionExtensions
+    {
+        public static float GetMean(this IEnumerable<float> numbers)
+        {
+            float total = 0;
+            int count = 0;
+
+            foreach (var number in numbers)
+            {
+                total += number;
+                count++;
+            }
+
+            return total / count;
+        }
+        public static float GetSum(this IEnumerable<float> numbers)
+        {
+            float total = 0;
+
+            foreach (var number in numbers)
+                total += number;
+
+            return total;
+        }
+
+        public static double GetMean(this IEnumerable<double> numbers)
+        {
+            double total = 0;
+            int count = 0;
+
+            foreach (var number in numbers)
+            {
+                total += number;
+                count++;
+            }
+
+            return total / count;
+        }
+        public static double GetSum(this IEnumerable<double> numbers)
+        {
+            double total = 0;
+
+            foreach (var number in numbers)
+                total += number;
+
+            return total;
+        }
+
+        public static int GetMean(this IEnumerable<int> numbers)
+        {
+            int total = 0;
+            int count = 0;
+
+            foreach (var number in numbers)
+            {
+                total += number;
+                count++;
+            }
+
+            return total / count;
+        }
+        public static int GetSum(this IEnumerable<int> numbers)
+        {
+            int total = 0;
+
+            foreach (var number in numbers)
+                total += number;
+
+            return total;
+        }
+
+        public static long GetMean(this IEnumerable<long> numbers)
+        {
+            long total = 0;
+            int count = 0;
+
+            foreach (var number in numbers)
+            {
+                total += number;
+                count++;
+            }
+
+            return total / count;
+        }
+        public static long GetSum(this IEnumerable<long> numbers)
+        {
+            long total = 0;
+
+            foreach (var number in numbers)
+                total += number;
+
+            return total;
+        }
+    }
+}
