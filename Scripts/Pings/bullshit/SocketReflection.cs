ee03f7fd74e9b41ecfdc523f634af72563255752
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-01-25 05:38:39 -0500

    Use reflection to call Socket.ReceiveFrom

---
+++ Scripts/Pings/bullshit/SocketReflection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Reflection;
+
+namespace JimmysUnityUtilities.Pings.Bullshit
+{
+    internal static class SocketReflection
+    {
+        // Socket has like 30 overloads for RecieveFrom, and of course Ping uses the ONE overload that isn't public.
+        // This was a bitch to figureo ut with the ref type and the out parameter.
+
+        private static readonly MethodInfo MethodInfo;
+
+        static SocketReflection()
+        {
+            var socketType = typeof(Socket);
+
+            MethodInfo = socketType.GetMethod("RecieveFrom", new Type[] { typeof(byte[]), typeof(int), typeof(int), typeof(SocketFlags), typeof(EndPoint).MakeByRefType(), typeof(SocketError) });
+        }
+
+
+        public static int ReceiveFrom(this Socket socket, byte[] buffer, int offset, int size, SocketFlags socketFlags, ref EndPoint remoteEP, out SocketError errorCode)
+        {
+            var parameters = new object[] { buffer, offset, size, socketFlags, remoteEP, null };
+            int value = (int)MethodInfo.Invoke(socket, parameters);
+
+            errorCode = (SocketError)parameters[5];
+            return value;
+        }
+    }
+}
