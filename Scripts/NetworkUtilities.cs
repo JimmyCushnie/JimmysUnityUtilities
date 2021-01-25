f3d489d25613e3ff2d2e44613fdf7e3ef8f41298
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-01-25 01:55:23 -0500

    NetworkUtilities parse methods now support IPv6

--- Scripts/NetworkUtilities.cs
+++ Scripts/NetworkUtilities.cs
@@ -4,6 +4,7 @@ using System.Net.NetworkInformation;
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
+using System.Globalization;
 
 namespace JimmysUnityUtilities
 {
@@ -25,16 +26,15 @@ namespace JimmysUnityUtilities
         }
 
 
-        // Todo: support ipv6 with the below methods
 
         /// <summary>
         /// Like <see cref="ParseServerEndpoint(string, int)"/> but with graceful failure handling.
         /// </summary>
-        public static bool TryParseServerEndpoint(string address, int defaultPort, out IPEndPoint endpoint)
+        public static bool TryParseServerEndpoint(string source, int defaultPort, out IPEndPoint endpoint)
         {
             try
             {
-                endpoint = ParseServerEndpoint(address, defaultPort);
+                endpoint = ParseServerEndpoint(source, defaultPort);
                 return true;
             }
             catch
@@ -45,26 +45,36 @@ namespace JimmysUnityUtilities
         }
 
         /// <summary>
-        /// Parse a string that refers to an IP endpoint in the format ip:port. This is the kind of string a user might enter to connect to the server.
+        /// Parse a string that refers to an IP endpoint in the format ip:port or [ip]:port (for IPv6). This is the kind of string a user might enter to connect to the server.
         /// </summary>
         /// <param name="defaultPort">The port that will be returned if <paramref name="address"/> does not contain a port.</param>
-        public static IPEndPoint ParseServerEndpoint(string address, int defaultPort)
+        public static IPEndPoint ParseServerEndpoint(string source, int defaultPort)
         {
-            string ip = address;
-            int port = defaultPort;
+            int addressLength = source.Length;
+            int lastColonIndex = source.LastIndexOf(':');
 
-            if (address.Contains(":"))
+            if (lastColonIndex > 0)
             {
-                var parts = address.Split(':');
-
-                if (parts.Length != 2)
-                    throw new FormatException($"Cannot parse {address} as {nameof(IPEndPoint)}");
-
-                ip = parts[0];
-                port = int.Parse(parts[1]);
+                if (source[lastColonIndex - 1] == ']')
+                {
+                    // This is an IPv6 address with a port
+                    addressLength = lastColonIndex;
+                }
+                else if (source.IndexOf(':') == lastColonIndex)
+                {
+                    // This is an IPv4 address with a port
+                    addressLength = lastColonIndex;
+                }
             }
 
-            return new IPEndPoint(ParseServerIP(ip), port);
+            string addressString = source.Substring(0, addressLength);
+            IPAddress address = ParseServerIP(addressString);
+
+            int port = defaultPort;
+            if (source.Length > addressLength)
+                port = int.Parse(source.Substring(addressLength + 1), NumberStyles.None, CultureInfo.InvariantCulture);
+
+            return new IPEndPoint(address, port);
         }
 
         /// <summary>
@@ -85,7 +95,7 @@ namespace JimmysUnityUtilities
         }
 
         /// <summary>
-        /// Convert a string to an ip address. Handles numbered IPs, hostname ips, and the "localhost" case.
+        /// Convert a string to an ip address. Handles standard format IPv4 and IPv6 addresses, hostnames, and the "localhost" case.
         /// </summary>
         /// <remarks>
         /// If a string is entered that is not a valid IP, this method will take 5 entire seconds to fail the DNS 
@@ -94,12 +104,9 @@ namespace JimmysUnityUtilities
         /// </remarks>
         public static IPAddress ParseServerIP(string ip)
         {
-            if (ip.Contains(":")) // If user passes string with port, handle that case and return the IP
-                return ParseServerEndpoint(ip, 0).Address;
-
             if (ip == "localhost")
-                return IPAddress.Loopback;
-            
+                return IPAddress.Loopback; // Maybe this should be IPAddress.IPv6Loopback ?
+
             if (IPAddress.TryParse(ip, out var ipAddress))
                 return ipAddress;
 
@@ -107,7 +114,7 @@ namespace JimmysUnityUtilities
             {
                 var hostEntry = Dns.GetHostEntry(ip); // This will time out after 5 seconds (not configurable)
                 if (hostEntry.AddressList.Length > 0)
-                    return hostEntry.AddressList[0];
+                    return hostEntry.AddressList[0]; // I'm not sure what should happen if a host has multiple addresses. This works for now ¯\_(ツ)_/¯
             }
             catch (SocketException)
             {
