a5ff3b05b995233c2027d4820169bf6dd4f06df1
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-01-31 01:28:42 -0500

    Added code for LocalNetworkBroadcaster

---
+++ Scripts/Networking/Broadcasting/BoundBroadcaster.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace JimmysUnityUtilities.Networking.Broadcasting
+{
+    /// <summary>
+    /// Internal class used by <see cref="LocalNetworkCommunicator"/>
+    /// </summary>
+    internal class BoundBroadcaster : IDisposable
+    {
+        public static bool TryCreate(IPAddress localAddress, int port, out BoundBroadcaster broadcaster)
+        {
+            try
+            {
+                broadcaster = new BoundBroadcaster(localAddress, port);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (ex is SocketException || ex is NotSupportedException)
+                {
+                    broadcaster = null;
+                    return false;
+                }
+
+                throw;
+            }
+        }
+
+        public int Port { get; }
+        private UdpClient Client { get; }
+
+        private BoundBroadcaster(IPAddress localAddress, int port)
+        {
+            // I have spent so much god damn time trying to get this piece of shit to work with IPv6 using ff02::1. Sorry, I just couldn't figure it out.
+            // It should be fine for the next couple decades, as essentially every local network in the world supports IPv4 and that's not liable to
+            // change any time soon.
+            // TODO: figure out IPv6!
+            if (localAddress.AddressFamily == AddressFamily.InterNetworkV6)
+                throw new NotSupportedException($"{nameof(BoundBroadcaster)} does not support IPv6. The local address must be IPv4.");
+
+
+            Port = port;
+
+            Client = new UdpClient();
+
+            var socket = Client.Client;
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            socket.ExclusiveAddressUse = false;
+
+            socket.Bind(new IPEndPoint(localAddress, Port));
+            BroadcastEndPoint = new IPEndPoint(IPAddress.Broadcast, port);
+        }
+
+        private readonly IPEndPoint BroadcastEndPoint;
+        public void Send(byte[] data)
+        {
+            Client.Send(data, data.Length, BroadcastEndPoint);
+        }
+
+        bool ReceiveThreadRunning = false;
+        public void StartRecievingData(Action<UdpReceiveResult> onDataRecieved)
+        {
+            if (ReceiveThreadRunning)
+                throw new InvalidOperationException("A data receiving thread is already running");
+
+            Task.Run(async () =>
+            {
+                while (!this.IsDisposed)
+                {
+                    var receivedResults = await Client.ReceiveAsync();
+                    onDataRecieved.Invoke(receivedResults);
+                }
+            });
+
+            ReceiveThreadRunning = true;
+        }
+
+        private bool IsDisposed = false;
+        public void Dispose()
+        {
+            Client.Dispose();
+            IsDisposed = true;
+        }
+    }
+}
---
+++ Scripts/Networking/Broadcasting/LocalNetworkBroadcaster.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace JimmysUnityUtilities.Networking.Broadcasting
+{
+    /// <summary>
+    /// Broadcast data, and listen for broadcasted data, with all other <see cref="LocalNetworkCommunicator"/>s on a local network 
+    /// that this device is connected to.
+    /// </summary>
+    /// <remarks>
+    /// This will receive messages it sends.
+    /// If the device has multiple network interfaces that allow broadcasting, there can be duplicate messages. If that matters
+    /// to your use case, make sure you check for duplicates.
+    /// </remarks>
+    public class LocalNetworkCommunicator : IDisposable
+    {
+        private readonly List<BoundBroadcaster> AllBroadcasters = new List<BoundBroadcaster>();
+
+        public LocalNetworkCommunicator(int port)
+        {
+            foreach (var localAddress in NetworkUtilities.GetAllLocalNetworkAddressesOfThisDevice())
+            {
+                if (BoundBroadcaster.TryCreate(localAddress, port, out var broadcaster))
+                {
+                    AllBroadcasters.Add(broadcaster);
+
+                    // It's unecessary to start the data recieve threads even when the use case might not require the ability to
+                    // receive data. If there are performance problems, this could be optimized by only calling StartRecievingData
+                    // on the broadcasters when someone subscribes to the OnDataReceived event.
+                    broadcaster.StartRecievingData(OnBroadcastDataReceived);
+                }
+            }
+        }
+
+        public Action<UdpReceiveResult> OnDataReceived;
+        private void OnBroadcastDataReceived(UdpReceiveResult result)
+        {
+            OnDataReceived?.Invoke(result);
+        }
+
+        public void Broadcast(byte[] data)
+        {
+            foreach (var broadcaster in AllBroadcasters)
+                broadcaster.Send(data);
+        }
+
+        public void Dispose()
+        {
+            foreach (var broadcaster in AllBroadcasters)
+                broadcaster.Dispose();
+        }
+    }
+}
