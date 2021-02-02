e24af71085db2c4f8b621ff0674c8f93cf9433d8
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-02-02 05:42:50 -0500

    Added extension to fix TMP shenanigans

---
+++ Scripts/Extensions/misc Unity types/TMP_InputFieldExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+
+namespace JimmysUnityUtilities
+{
+    public static class TMP_InputFieldExtensions
+    {
+        /// <summary>
+        /// TextMeshPro does this brilliant thing where it triggers the <see cref="TMP_InputField.onSubmit"/> event both when you press
+        /// enter to submit, and also when you press escape to cancel. If you subscribe to the event using this extension method instead,
+        /// your callback will only be fired when the input field is ACTUALLY submitted (i.e. the user presses enter and not escape).
+        /// </summary>
+        public static void OnActuallySubmitted(this TMP_InputField inputField, Action<string> onActuallySubmittedCallback)
+        {
+            inputField.onSubmit.AddListener(text =>
+            {
+                if (!inputField.wasCanceled)
+                    onActuallySubmittedCallback?.Invoke(text);
+            });
+        }
+    }
+}
