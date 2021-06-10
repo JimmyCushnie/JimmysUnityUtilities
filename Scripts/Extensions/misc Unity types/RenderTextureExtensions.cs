10c2338febf02b5d1adefe152fbb2cd315f33a1f
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-06-10 02:44:54 -0400

    Converting a RenderTexture to a Texture2D now supports transparency by default

--- Scripts/Extensions/misc Unity types/RenderTextureExtensions.cs
+++ Scripts/Extensions/misc Unity types/RenderTextureExtensions.cs
@@ -4,12 +4,12 @@ namespace JimmysUnityUtilities
 {
     public static class RenderTextureExtensions
     {
-        public static Texture2D ToTexture2D(this RenderTexture source)
+        public static Texture2D ToTexture2D(this RenderTexture source, bool supportTransparency = true)
         {
             var previouslyActiveRenderTexture = RenderTexture.active;
             RenderTexture.active = source;
 
-            var texture = new Texture2D(source.width, source.height, TextureFormat.RGB24, false);
+            var texture = new Texture2D(source.width, source.height, supportTransparency ? TextureFormat.RGBA32 : TextureFormat.RGB24, false);
             texture.ReadPixels(new Rect(0, 0, source.width, source.height), 0, 0);
             texture.Apply();
 
