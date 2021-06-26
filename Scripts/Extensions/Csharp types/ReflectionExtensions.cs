c0adeb3bc6d9c7bed5d69e49db54af4e8860c6dd
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-06-26 16:17:37 -0400

    Add GetExtensionMethods extension method (woah)

---
+++ Scripts/Extensions/Csharp types/ReflectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace JimmysUnityUtilities
+{
+    public static class ReflectionExtensions
+    {
+        /// <summary>
+        /// Search all loaded assemblies for extension methods on a given type.
+        /// </summary>
+        public static IEnumerable<MethodInfo> GetExtensionMethods(this Type type)
+        {
+            var loadedTypes = new List<Type>();
+            foreach (Assembly ass in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                loadedTypes.AddRange(ass.GetTypes());
+            }
+
+            var query = 
+                from loadedType in loadedTypes
+                where loadedType.IsSealed && !loadedType.IsGenericType && !loadedType.IsNested
+                from method in loadedType.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                where method.IsDefined(typeof(ExtensionAttribute), false)
+                where method.GetParameters()[0].ParameterType == type
+                select method;
+
+            return query;
+        }
+    }
+}
