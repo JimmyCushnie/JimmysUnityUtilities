d3febf711ce9a4461a1490bef839f2dd0df87c00
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-09-05 04:24:55 -0400

    oh right, this constant has a name lol

--- Scripts/Random/JRandom.Geometry.cs
+++ Scripts/Random/JRandom.Geometry.cs
@@ -4,8 +4,8 @@ namespace JimmysUnityUtilities.Random
 {
     public partial class JRandom
     {
-        const double TwoPi = 6.2831853071795864769252867665590057683943387987502116419498891846156328125724179972560696506842341359642962;
-        const float TwoPiF = (float)TwoPi;
+        const double Tau = 6.2831853071795864769252867665590057683943387987502116419498891846156328125724179972560696506842341359642962;
+        const float TauF = (float)Tau;
 
 
         /// <summary> Gets a random single-precision value in the range [0, 360]. </summary>
@@ -16,13 +16,13 @@ namespace JimmysUnityUtilities.Random
         public double AngleDegreesD()
             => Range(0d, 360d);
 
-        /// <summary> Gets a random single-precision value in the range [0, 2*pi]. </summary>
+        /// <summary> Gets a random single-precision value in the range [0, tau]. </summary>
         public float AngleRadians()
-            => Range(0f, TwoPiF);
+            => Range(0f, TauF);
 
-        /// <summary> Gets a random double-precision value in the range [0, 2*pi]. </summary>
+        /// <summary> Gets a random double-precision value in the range [0, tau]. </summary>
         public double AngleRadiansD()
-            => Range(0d, TwoPi);
+            => Range(0d, Tau);
 
 
         /// <summary> Gets a random point on the edge of a circle ceneterd on (0, 0) with a radius of 1. </summary>
