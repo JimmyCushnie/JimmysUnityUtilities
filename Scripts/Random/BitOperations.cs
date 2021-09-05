118fb1c4943fac628e642bb82c00423c46452416
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-09-05 06:31:12 -0400

    improve BitOperations, bleh

--- Scripts/Random/BitOperations.cs
+++ Scripts/Random/BitOperations.cs
@@ -1,86 +1,32 @@
 ﻿
-// The version of .NET used by Unity doesn't have the BitOperations class, so I copied the source code from .NET 6 here with the methods I need.
-// This will be slower than using the .NET BitOperations, since we can't use hardware intrinsics.
+// The version of .NET used by Unity doesn't have the BitOperations class. I need some methods from it for the JRandom stuff,
+// so I provided implementations of them here, as fast as I can get them.
+// Todo: once Unity finally ditches mono, use the much faster native .NET BitOperations.
 // -Jimmy
-
-
-
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// Some routines inspired by the Stanford Bit Twiddling Hacks by Sean Eron Anderson:
-// http://graphics.stanford.edu/~seander/bithacks.html
 
 namespace JimmysUnityUtilities.Random
 {
     internal static class BitOperations
     {
-        /// <summary>
-        /// Returns the integer (floor) log of the specified value, base 2.
-        /// Note that by convention, input value 0 returns 0 since log(0) is undefined.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        [CLSCompliant(false)]
-        public static int Log2(uint value)
+        //https://stackoverflow.com/questions/10439242/count-leading-zeroes-in-an-int32/10439333#10439333
+        //https://stackoverflow.com/questions/10439242/count-leading-zeroes-in-an-int32/10439333#10439333
+        //https://github.com/SunsetQuest/Fast-Integer-Log2/blob/master/BenchmarkLeading0Count/Program.cs#L583
+        //fuck
+        public static int Log2(ulong x)
         {
-            // No AggressiveInlining due to large method size
-            // Has conventional contract 0->0 (Log(0) is undefined)
-
-            // Fill trailing zeros with ones, eg 00010010 becomes 00011111
-            value |= value >> 01;
-            value |= value >> 02;
-            value |= value >> 04;
-            value |= value >> 08;
-            value |= value >> 16;
-
-            // uint.MaxValue >> 27 is always in range [0 - 31] so we use Unsafe.AddByteOffset to avoid bounds check
-            return Unsafe.AddByteOffset(
-                // Using deBruijn sequence, k=2, n=5 (2^5=32) : 0b_0000_0111_1100_0100_1010_1100_1101_1101u
-                ref MemoryMarshal.GetReference(Log2DeBruijn),
-                // uint|long -> IntPtr cast on 32-bit platforms does expensive overflow checks not needed here
-                (IntPtr)(int)((value * 0x07C4ACDDu) >> 27));
-        }
-        private static ReadOnlySpan<byte> Log2DeBruijn => new byte[32]
-        {
-            00, 09, 01, 10, 13, 21, 02, 29,
-            11, 14, 16, 18, 22, 25, 03, 30,
-            08, 12, 20, 28, 15, 17, 24, 07,
-            19, 27, 23, 06, 26, 05, 04, 31
-        };
-
-        /// <summary>
-        /// Returns the integer (floor) log of the specified value, base 2.
-        /// Note that by convention, input value 0 returns 0 since log(0) is undefined.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Log2(ulong value)
-        {
-            value |= 1;
-
-            uint hi = (uint)(value >> 32);
-
-            if (hi == 0)
-            {
-                return Log2((uint)value);
-            }
-
-            return 32 + Log2(hi);
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+            x -= x >> 1 & 0x55555555;
+            x = (x >> 2 & 0x33333333) + (x & 0x33333333);
+            x = (x >> 4) + x & 0x0f0f0f0f;
+            x += x >> 8;
+            x += x >> 16;
+            return (int)((x & 0x0000003f) - 1);
         }
 
-        /// <summary>
-        /// Rotates the specified value left by the specified number of bits.
-        /// Similar in behavior to the x86 instruction ROL.
-        /// </summary>
-        /// <param name="value">The value to rotate.</param>
-        /// <param name="offset">The number of bits to rotate by.
-        /// Any value outside the range [0..63] is treated as congruent mod 64.</param>
-        /// <returns>The rotated value.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        [CLSCompliant(false)]
         public static ulong RotateLeft(ulong value, int offset)
             => (value << offset) | (value >> (64 - offset));
     }
