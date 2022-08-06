f4306bcd23eeb46c530a094031bf1fe6554f2251
Jimmy Cushnie <jimmy@mousehatgames.com>
2022-08-06 18:19:34 -0400

    add Material.Clone() extension

--- Scripts/Extensions/misc Unity types/MaterialExtensions.cs
+++ Scripts/Extensions/misc Unity types/MaterialExtensions.cs
@@ -5,7 +5,7 @@ namespace JimmysUnityUtilities
 {
     public static class MaterialExtensions
     {
-        // this is just so the modifications to Vector1 line up with modifications to Vector2/3/4
+        // this one is just so the modifications to Vector1 line up with modifications to Vector2/3/4
         public static void SetVector(this Material material, string name, float value)
             => material.SetFloat(name, value);
 
@@ -26,5 +26,16 @@ namespace JimmysUnityUtilities
 
         public static void SetBoolean(this Material material, string name, bool value)
             => SetVector(material, name, value ? 1 : 0);
+
+
+        /// <summary>
+        /// The Unity function for this (<see cref="Material(Material)"/>) does not copy shader keywords when cloning runtime-created materials. This method works properly, however.
+        /// </summary>
+        public static Material Clone(this Material template)
+        {
+            var clone = new Material(template);
+            clone.shaderKeywords = template.shaderKeywords;
+            return clone;
+        }
     }
 }
