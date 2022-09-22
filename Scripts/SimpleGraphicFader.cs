bf10cfc9a76396b5201750d27bec417aea4c5fc6
Jimmy Cushnie <jimmy@mousehatgames.com>
2022-09-22 16:19:16 -0400

    added SimpleGraphicFader

---
+++ Scripts/SimpleGraphicFader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace JimmysUnityUtilities
+{
+    [RequireComponent(typeof(Graphic))]
+    public class SimpleGraphicFader : MonoBehaviour
+    {
+        public float PeriodSeconds = 1;
+
+        [Range(0f, 1f)]
+        public float MinAlpha = 0;
+
+        [Range(0f, 1f)]
+        public float MaxAlpha = 1;
+
+
+        private void Awake()
+        {
+            Graphic = GetComponent<Graphic>();
+        }
+        private Graphic Graphic;
+
+        private void Update()
+        {
+            const float tau = Mathf.PI / 2f;
+            float coefficient = tau / PeriodSeconds;
+
+            float alpha = MinAlpha + Mathf.Sin(Time.time * coefficient) * (MaxAlpha - MinAlpha);
+            Graphic.SetAlpha(alpha);
+        }
+    }
+}
