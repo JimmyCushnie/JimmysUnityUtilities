5c80c39bc4dd2de2ffa9a599ccfe83b0dd834341
Jimmy Cushnie <jimmy@mousehatgames.com>
2023-01-15 03:09:36 -0500

    JRandom housekeeping
    removed stupid constructors

--- Scripts/Random/JRandom.Core.cs
+++ Scripts/Random/JRandom.Core.cs
@@ -3,39 +3,23 @@
 namespace JimmysUnityUtilities.Random
 {
     /// <summary>
-    /// Seeded random generation, with more built-in methods than <see cref="System.Random"/>.
+    /// Seeded random value generation, with more built-in methods than <see cref="System.Random"/>.
     /// </summary>
     public partial class JRandom
     {
-        /// <summary>
-        /// Creates a new <see cref="JRandom"/> with a pseudorandom seed.
-        /// </summary>
-        public JRandom() : this(GetPseudorandomSeed())
-        {
-        }
-        static ulong GetPseudorandomSeed()
-            => (ulong)DateTime.Now.Ticks;
-
-        /// <summary>
-        /// Creates a new seeded <see cref="JRandom"/>, using the hashcode of the provided object.
-        /// </summary>
-        public JRandom(object objectSeed) : this(objectSeed.GetHashCode())
-        {
-        }
-
-        /// <summary>
-        /// Creates a new <see cref="JRandom"/> with a 32-bit seed.
-        /// </summary>
-        public JRandom(int seed) : this((ulong)seed)
-        {
-        }
-
         /// <summary>
         /// Creates a new <see cref="JRandom"/> with a 64-bit seed.
         /// </summary>
         public JRandom(ulong seed)
         {
             _Generator = new VeryFastRandomValueGenerator(seed);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="JRandom"/> with a pseudorandom seed.
+        /// </summary>
+        public JRandom() : this(Shared.UInt64())
+        {
         }
 
 
