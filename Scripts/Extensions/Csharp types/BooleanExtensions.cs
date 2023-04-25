1f37ba29808e94443b61636c529692be1511780d
Jimmy Cushnie <jimmy@mousehatgames.com>
2023-04-25 02:31:57 -0400

    Create BooleanExtensions

---
+++ Scripts/Extensions/Csharp types/BooleanExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace JimmysUnityUtilities
+{
+    public static class BooleanExtensions
+    {
+        /// <summary> If true, returns 1; if false, returns -1 </summary>
+        public static int Sign(this bool value)
+            => value ? 1 : -1;
+
+        /// <summary> If true, returns 1; if false, returns 0 </summary>
+        public static int Bit(this bool value)
+            => value ? 1 : 0;
+    }
+}
