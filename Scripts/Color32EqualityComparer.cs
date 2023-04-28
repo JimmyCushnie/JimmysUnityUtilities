da11f6bd18f2ed254cbe5845b7961a435b8b88f7
Jimmy Cushnie <jimmy@mousehatgames.com>
2023-04-28 10:51:47 -0400

    housekeeping
    derive from EqualityComparer, instead of implementing IEqualityComparer, in accordance with the recommended best practices (https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.equalitycomparer-1)

--- Scripts/Color32EqualityComparer.cs
+++ Scripts/Color32EqualityComparer.cs
@@ -6,14 +6,14 @@ namespace JimmysUnityUtilities
     // Because Unity are negligent fools, they did not include a built-in == operator or .Equals() method for Color32.
     // However, SOMETIMES YOU NEED TO FREAKING COMPARE TWO COLOR32s.
 
-    public class Color32EqualityComparer : IEqualityComparer<Color32>
+    public class Color32EqualityComparer : EqualityComparer<Color32>
     {
         public static Color32EqualityComparer Instance { get; } = new Color32EqualityComparer();
 
-        public bool Equals(Color32 x, Color32 y)
+        public override bool Equals(Color32 x, Color32 y)
             => x.r == y.r && x.g == y.g && x.b == y.b && x.a == y.a;
 
-        public int GetHashCode(Color32 obj)
+        public override int GetHashCode(Color32 obj)
             => (((13 * 7 + obj.r) * 7 + obj.g) * 7 + obj.b) * 7 + obj.a;
     }
 }
