1df6155e6e78a1ecdff05a1b52c04e97adc9dcae
Jimmy Cushnie <jimmy@mousehatgames.com>
2023-05-15 07:10:39 -0400

    add FileUtilities.IsIllegalFileSystemPath

--- Scripts/FileUtilities.cs
+++ Scripts/FileUtilities.cs
@@ -17,13 +17,36 @@ namespace JimmysUnityUtilities
             => DateTime.TryParseExact(timeStamp, FileTimestampFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
 
 
-        /// <summary> replaces any characters that cannot be in a file name </summary>
-        /// <param name="replacement"> invalid characters will be replaced with this character </param>
-        public static string ValidatedFileName(string name, char replacement = '_')
+        static readonly IReadOnlyList<string> IllegalWindowsFileNames = new string[]
+        {
+            "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
+        };
+        public static bool IsIllegalFileSystemPath(string path)
         {
             // https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file
             // Todo support the rest of this nonsense
 
+            if (path.Any(c => Path.GetInvalidPathChars().Contains(c)))
+                return true;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                string fileName = Path.GetFileName(path);
+                foreach (var illegalName in IllegalWindowsFileNames)
+                {
+                    if (fileName.Equals(illegalName, StringComparison.OrdinalIgnoreCase) ||
+                        fileName.StartsWith(illegalName + '.', StringComparison.OrdinalIgnoreCase))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary> replaces any characters that cannot be in a file name </summary>
+        /// <param name="replacement"> invalid characters will be replaced with this character </param>
+        public static string ValidatedFileName(string name, char replacement = '_')
+        {
             if (name.IsNullOrEmpty())
                 name = replacement.ToString();
 
@@ -41,11 +64,7 @@ namespace JimmysUnityUtilities
 
             return validatedName;
         }
-        static string[] IllegalWindowsFileNames = new string[]
-        {
-            "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
-        };
-        
+
         /// <summary>
         /// You provide a parent directory and a desired name. We return that name, modified if necessary to make sure it is a valid path name and doesn't already exist.
         /// </summary>
