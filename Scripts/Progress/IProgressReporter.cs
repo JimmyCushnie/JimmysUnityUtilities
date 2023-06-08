dab80934614c90cd5ecf9ac5165ffe69c84e6c10
Jimmy Cushnie <jimmy@mousehatgames.com>
2023-06-07 22:39:12 -0400

    added ProgressReporter feature

---
+++ Scripts/Progress/IProgressReporter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace JimmysUnityUtilities.Progress
+{
+    public interface IProgressReporter
+    {
+        void SetCompletionFraction(float completionFraction);
+        void SetStatusMessage(string statusMessage);
+
+        void FinishedSuccessfully();
+        void FinishedWithError(Exception error = null);
+
+        bool HasFinishedSuccessfully { get; }
+        bool HasFinishedWithError { get; }
+    }
+}
---
+++ Scripts/Progress/UnityMainThreadProgressReporter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace JimmysUnityUtilities.Progress
+{
+    public class UnityMainThreadProgressReporter : IProgressReporter
+    {
+        public bool HasFinishedSuccessfully { get; private set; } = false;
+        public void FinishedSuccessfully()
+        {
+            HasFinishedSuccessfully = true;
+            Dispatcher.InvokeAsync(() => OnFinishedSuccessfully?.Invoke());
+        }
+        private readonly Action OnFinishedSuccessfully;
+
+        public bool HasFinishedWithError { get; private set; } = false;
+        public void FinishedWithError(Exception error)
+        {
+            HasFinishedWithError = true;
+            Dispatcher.InvokeAsync(() => OnFinishedWithError?.Invoke(error));
+        }
+        private readonly Action<Exception> OnFinishedWithError;
+
+        public void SetCompletionFraction(float completionFraction) => Dispatcher.InvokeAsync(() => OnSetCompletionFraction?.Invoke(completionFraction));
+        private readonly Action<float> OnSetCompletionFraction;
+
+        public void SetStatusMessage(string statusMessage) => Dispatcher.InvokeAsync(() => OnSetStatusMessage?.Invoke(statusMessage));
+        private readonly Action<string> OnSetStatusMessage;
+
+
+        public UnityMainThreadProgressReporter(Action onFinishedSuccessfully, Action<Exception> onFinishedWithError, Action<float> onSetCompletionFraction, Action<string> onSetStatusMessage)
+        {
+            OnFinishedSuccessfully = onFinishedSuccessfully;
+            OnFinishedWithError = onFinishedWithError;
+            OnSetCompletionFraction = onSetCompletionFraction;
+            OnSetStatusMessage = onSetStatusMessage;
+        }
+    }
+}
