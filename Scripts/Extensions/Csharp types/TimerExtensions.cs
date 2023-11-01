f5060e6b3eedc1b2334fd24433b27616716002c5
Jimmy Cushnie <jimmy@mousehatgames.com>
2023-11-01 00:03:14 -0400

    Add TimerExtensions

---
+++ Scripts/Extensions/Csharp types/TimerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Timers;
+
+namespace JimmysUnityUtilities
+{
+    /// <summary>
+    /// Enables the use of <see cref="TimeSpan"/> when getting or setting the interval of a <see cref="Timer"/>.
+    /// </summary>
+    public static class TimerExtensions
+    {
+        public static TimeSpan GetInterval(this Timer timer)
+        {
+            return TimeSpan.FromMilliseconds(timer.Interval);
+        }
+
+        public static void SetInterval(this Timer timer, TimeSpan interval)
+        {
+            timer.Interval = interval.TotalMilliseconds;
+        }
+    }
+}
