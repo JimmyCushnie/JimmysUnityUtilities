40396c47316a703f95dd15014b296cf804436841
Jimmy Cushnie <jimmy@mousehatgames.com>
2024-01-10 17:13:11 -0500

    Add some more random string methods

--- Scripts/Random/JRandom.Strings.cs
+++ Scripts/Random/JRandom.Strings.cs
@@ -4,20 +4,31 @@ namespace JimmysUnityUtilities.Random
 {
     public partial class JRandom
     {
-        public string UppercaseAlphanumericString(int length)
+        private string GenerateRandomString(char[] sourceChars, int length)
         {
-            const string uppercaseAlphanumericCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
             var sb = new StringBuilder(length);
 
             for (int i = 0; i < length; i++)
             {
-                char nextChar = uppercaseAlphanumericCharacters[this.Range(0, uppercaseAlphanumericCharacters.Length - 1)];
+                char nextChar = this.RandomElementOf(sourceChars);
                 sb.Append(nextChar);
             }
 
             return sb.ToString();
         }
+
+
+        private static readonly char[] UppercaseAlphanumericCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+        public string UppercaseAlphanumericString(int length)
+            => GenerateRandomString(UppercaseAlphanumericCharacters, length);
+
+        private static readonly char[] LowercaseAlphanumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray();
+        public string LowercaseAlphanumericString(int length)
+            => GenerateRandomString(LowercaseAlphanumericCharacters, length);
+
+        private static readonly char[] MixedCaseAlphanumericCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".ToCharArray();
+        public string MixedCaseAlphanumericString(int length)
+            => GenerateRandomString(MixedCaseAlphanumericCharacters, length);
 
         // todo more useful string methods
         // todo generate random strings of emojis lmao
