d1303c7b4e0f40f321e16ed488c70202137c5e78
Jimmy Cushnie <jimmy@mousehatgames.com>
2024-01-16 02:43:35 -0500

    Add TimeUtilities

---
+++ Scripts/TimeUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JimmysUnityUtilities
+{
+    public static class TimeUtilities
+    {
+        /// <summary>
+        /// Gets a string representing the offset of <see cref="time"/> from UTC (i.e. "UTC-03:00", "UTC+05:30", etc.)
+        /// </summary>
+        public static string GetUtcOffsetString(DateTimeOffset time)
+        {
+            string utcOffset = time.Offset.ToString("hh\\:mm");
+            return (time.Offset >= TimeSpan.Zero ? "UTC+" : "UTC-") + utcOffset;
+        }
+    }
+}
