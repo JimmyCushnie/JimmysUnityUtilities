89bcc13934153e714125f36b099fa256f92b10b5
Jimmy Cushnie <git@jimmycushnie.com>
2024-08-22 19:52:07 -0400

    Create ListedDictionary.cs

---
+++ Scripts/Collections/ListedDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JimmysUnityUtilities.Collections
+{
+    /// <summary>
+    /// Like <see cref="Dictionary{TKey,TValue}"/>, but it can have multiple <see cref="TValue"/>s per <see cref="TKey"/>
+    /// Somewhat limited functionality, for now
+    /// </summary>
+    public class ListedDictionary<TKey, TValue>
+    {
+        private readonly Dictionary<TKey, List<TValue>> BackingDictionary = new Dictionary<TKey, List<TValue>>();
+
+        public void AddAt(TKey key, TValue value)
+        {
+            if (BackingDictionary.TryGetValue(key, out var list))
+            {
+                list.Add(value);
+            }
+            else
+            {
+                BackingDictionary[key] = new List<TValue> { value };
+            }
+        }
+
+        public IList<TValue> GetAt(TKey key)
+        {
+            if (BackingDictionary.TryGetValue(key, out var list))
+                return list;
+
+            return Array.Empty<TValue>();
+        }
+
+        public void Clear()
+        {
+            BackingDictionary.Clear();
+        }
+
+        public void ClearAt(TKey key)
+        {
+            BackingDictionary.Remove(key);
+        }
+    }
+}
